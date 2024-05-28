a80087eb2feddbce892c80fd300ebd5f9e1ff1dd
jayou2323 <jayou1223@gmail.com>
2024-05-28 21:40:20 +0900

    준비 중단시 alert창 띄어주던거 비활성화

--- prototype/src/screens/Onboard/Shower.js
+++ prototype/src/screens/Onboard/Shower.js
@@ -46,30 +46,30 @@ export default function Shower(){
           console.log('AppState', appState.current);
         });
 
-        navigation.addListener('beforeRemove', (e) => {
-            // Prevent default behavior of leaving the screen
-            e.preventDefault();
+        // navigation.addListener('beforeRemove', (e) => {
+        //     // Prevent default behavior of leaving the screen
+        //     e.preventDefault();
     
-            // Prompt the user before leaving the screen
-            // Alert.alert(
-            //   '준비를 그만두시겠습니까?',
-            //   '아직 준비를 완료하지 않으셨어요. 되돌아가시겠습니까?',
-            //   [
-            //     { text: "계속하기", style: 'cancel', onPress: () => {} },
-            //     {
-            //       text: '그만두기',
-            //       style: 'destructive',
-            //       // If the user confirmed, then we dispatch the action we blocked earlier
-            //       // This will continue the action that had triggered the removal of the screen
-            //       onPress: () => navigation.dispatch(e.data.action),
-            //     },
-            //   ]
-            // );
-        });
+        //     // Prompt the user before leaving the screen
+        //     Alert.alert(
+        //       '준비를 그만두시겠습니까?',
+        //       '아직 준비를 완료하지 않으셨어요. 되돌아가시겠습니까?',
+        //       [
+        //         { text: "계속하기", style: 'cancel', onPress: () => {} },
+        //         {
+        //           text: '그만두기',
+        //           style: 'destructive',
+        //           // If the user confirmed, then we dispatch the action we blocked earlier
+        //           // This will continue the action that had triggered the removal of the screen
+        //           onPress: () => navigation.dispatch(e.data.action),
+        //         },
+        //       ]
+        //     );
+        // });
     
         return () => {
           subscription.remove();
-          navigation.removeListener();
+        //   navigation.removeListener();
         };
       }, []);
 
